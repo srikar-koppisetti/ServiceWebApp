066343eb2e50f81cf360785ed9e21fc3d26241da
srikarslash <srikar.slash@gmail.com>
2018-03-21 23:08:12 -0400

    Add all components

--- service-web-app/src/app/app.module.ts
+++ service-web-app/src/app/app.module.ts
@@ -1,21 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ClientsComponent } from './clients/clients.component';
+import { FurnitureComponent } from './furniture/furniture.component';
+import { ContactComponent } from './contact/contact.component';
 
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'projects', component: ProjectsComponent},
+  { path: 'furniture', component: FurnitureComponent },
+  { path: 'contact', component: ContactComponent },
+  {path: 'clients', component: ClientsComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    ServicesComponent,
+    ProjectsComponent,
+    ClientsComponent,
+    FurnitureComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
